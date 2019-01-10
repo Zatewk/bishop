6d4ef0c7c429764ac906bb3d32497d4e5cb72b6a
Zatewk <dani.rmart@gmail.com>
2019-01-10 20:53:44 +0100

    lookup function created
    Let's start by implementing a function that will be called every time
    the user tries to access to the base url (/items), that will return some
    json code.
    In this function, we will request the data to the three end point, and
    we will normalize it.

--- controllers/items.js
+++ controllers/items.js
@@ -1,8 +1,19 @@
 const { Router } = require('express');
+const request = require('request');
+const app = require('../app');
+
+const lookupItem = (req, res) => {
+	request (
+		'http://www.clashapi.xyz/api/cards/', 
+		(error, response, body) => {
+			let info = JSON.parse (body);
+			res.send(info);
+		});
+}
 
 module.exports = new Router()
-  .get('/', (req, res) => {
-    // Code here
+  .get('/', lookupItem, (req, res) => {
+    res.json(req.info);
     res.sendStatus(200);
   })
   .get('/:id', (req, res) => {
