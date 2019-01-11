7b61398c815c8386b44d140c8105ad1f1afab377
Zatewk <dani.rmart@gmail.com>
2019-01-11 01:42:20 +0100

    Third Step: Getting the last list and concatenating
    This end point is weird...because there is no ID per se. We have the
    'driverId'... but in that case, should I use the 'nameId' in the card
    list?? this is confusing. Anyway, third list ready, and all
    concatenated. Next step: return the result to the browser when the
    content is requested.

--- controllers/items.js
+++ controllers/items.js
@@ -15,16 +15,32 @@ module.exports = new Router()
 		'http://www.clashapi.xyz/api/cards', 
 		(error, response, body) => {
 			let info = JSON.parse(body);
-			const cardIDs = info.map(card => {return {id:card._id, name:card.name}});
+			const cardIDs = info.map(card => {
+				return {id:card._id, name:card.name}
+			});
 			//console.log(cardIDs);
 
 			request (
 				'https://pokeapi.co/api/v2/pokemon/',
 				(error, response, body) => {
 					let obj = JSON.parse(body);
-					const pkmnIDs = obj.results.map(pkmn => {return {id:pkmn.url.slice(0, -1).split("/").pop(), name:pkmn.name}});
+					const pkmnIDs = obj.results.map(pkmn => {
+						return {id:pkmn.url.slice(0, -1).split("/").pop(), name:pkmn.name}
+					});
 					//console.log(pkmnIDs);
 
+					request (
+						'http://ergast.com/api/f1/drivers.json',
+						(error, response, body) => {
+							let obj = JSON.parse(body);
+							const driversIDs = obj.MRData.DriverTable.Drivers.map(driver => {
+								return {id:driver.driverId, name:driver.givenName+' '+driver.familyName}
+							});
+							//console.log(driversIDs);
+
+							const result = cardIDs.concat(pkmnIDs.concat(driversIDs));
+						}
+					);
 				}
 			);
 		}
