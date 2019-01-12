ebfc21ced2a3d9df8942a2314c3cf81e4782c7bc
Zatewk <dani.rmart@gmail.com>
2019-01-12 19:05:49 +0100

    First endpoint finished
    Same outcome, better implementation.
    In order to avoid the callback hell, I'm using promises with
    async/await. Everything works as expected.

--- controllers/items.js
+++ controllers/items.js
@@ -3,57 +3,39 @@ const request = require("request-promise");
 const app = require('../app');
 
 
+const apis = new Map([
+	["Clash","http://www.clashapi.xyz/api/cards"],
+	["Pokemon","https://pokeapi.co/api/v2/pokemon/"],
+	["Drivers","http://ergast.com/api/f1/drivers.json"]
+]);
 
+const lookupItems = async (req, res) => {
+	let rawCards = JSON.parse(await request (apis.get("Clash")));
+	let rawPkmns = JSON.parse(await request (apis.get("Pokemon")));
+	let rawDrivers = JSON.parse(await request (apis.get("Drivers")));
 
-const lookupItem = async (req, res) => {
-	let rawCards = JSON.parse(await request ('http://www.clashapi.xyz/api/cards'));
-	console.log(rawCards);
+	let cardIDs = rawCards.map(card => {
+		return {id:card._id, name:card.name}
+	});
+
+	let pkmnIDs = rawPkmns.results.map(pkmn => {
+		return {id:pkmn.url.slice(0, -1).split("/").pop(), name:pkmn.name}
+	});
+
+	let driversIDs = rawDrivers.MRData.DriverTable.Drivers.map(driver => {
+		return {id:driver.driverId, name:driver.givenName+' '+driver.familyName}
+	});
+
+	let result = cardIDs.concat(pkmnIDs.concat(driversIDs));
+	//console.log(rawCards);
+	res.send(JSON.stringify(result));
 }
-
 
 module.exports = new Router()
 
   .get('/', (req, res) => {
-    request (
-		'http://www.clashapi.xyz/api/cards', 
-		(error, response, body) => {
-			let info = JSON.parse(body);
-			const cardIDs = info.map(card => {
-				return {id:card._id, name:card.name}
-			});
-			//console.log(cardIDs);
-
-			request (
-				'https://pokeapi.co/api/v2/pokemon/',
-				(error, response, body) => {
-					let obj = JSON.parse(body);
-					const pkmnIDs = obj.results.map(pkmn => {
-						return {id:pkmn.url.slice(0, -1).split("/").pop(), name:pkmn.name}
-					});
-					//console.log(pkmnIDs);
-
-					request (
-						'http://ergast.com/api/f1/drivers.json',
-						(error, response, body) => {
-							let obj = JSON.parse(body);
-							const driversIDs = obj.MRData.DriverTable.Drivers.map(driver => {
-								return {id:driver.driverId, name:driver.givenName+' '+driver.familyName}
-							});
-							//console.log(driversIDs);
-
-							const result = cardIDs.concat(pkmnIDs.concat(driversIDs));
-							let resultJSON = JSON.stringify(result);
-							//console.log(resultJSON);
-							//response.send (resultJSON);
-
-						}
-					);
-				}
-			);
-		}
-	);
 	lookupItem(req, res);
-    res.sendStatus(200);
+    //res.sendStatus(200);
   })
 	
   .get('/:id', (req, res) => {
