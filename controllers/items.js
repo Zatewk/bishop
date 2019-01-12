e70863fd2881b609982b8237a4e176e36d89d9e9
Zatewk <dani.rmart@gmail.com>
2019-01-12 20:35:34 +0100

    Second endpoint: done
    it was easy to get. I had only one issue with the drivers api. It
    returns always something, even if the ID doesn't exists. So it was
    necessary to check an attribute in order to avoid issues.

--- controllers/items.js
+++ controllers/items.js
@@ -4,15 +4,16 @@ const app = require('../app');
 
 
 const apis = new Map([
-	["Clash","http://www.clashapi.xyz/api/cards"],
+	["Clash","http://www.clashapi.xyz/api/cards/"],
 	["Pokemon","https://pokeapi.co/api/v2/pokemon/"],
-	["Drivers","http://ergast.com/api/f1/drivers.json"]
+	["Drivers","http://ergast.com/api/f1/drivers"],
+	["Clash_Img","http://www.clashapi.xyz/images/cards/"]
 ]);
 
 const lookupItems = async (req, res) => {
 	let rawCards = JSON.parse(await request (apis.get("Clash")));
 	let rawPkmns = JSON.parse(await request (apis.get("Pokemon")));
-	let rawDrivers = JSON.parse(await request (apis.get("Drivers")));
+	let rawDrivers = JSON.parse(await request (apis.get("Drivers")+".json"));
 
 	let cardIDs = rawCards.map(card => {
 		return {id:card._id, name:card.name}
@@ -27,18 +28,51 @@ const lookupItems = async (req, res) => {
 	});
 
 	let result = cardIDs.concat(pkmnIDs.concat(driversIDs));
-	//console.log(rawCards);
+
+	res.send(JSON.stringify(result));
+}
+
+const lookupItemByID = async (req, res) => {
+	let raw;
+	let result;
+
+	// test ID: 5b099537ab411c001423ec3b
+	try{
+		raw = JSON.parse(await request (apis.get("Clash")+req.params.id));
+		result = {id:raw._id, image:apis.get("Clash_Img")+raw.idName+".png", name:raw.name};
+	}catch (err){
+		console.log("Card not found");
+	}
+
+	// test ID: 1
+	try{
+		raw = JSON.parse(await request (apis.get("Pokemon")+req.params.id));
+		result = {id:raw.id, image:raw.sprites.front_default, name:raw.name};
+	}catch (err){
+		console.log("Pokemon not found");
+	}
+	
+	// test ID: abate
+	try{
+		raw = JSON.parse(await request (apis.get("Drivers")+"/"+req.params.id+".json"));
+		if (raw.MRData.total == 1){
+			result = raw.MRData.DriverTable.Drivers.map(driver => {
+				return {id:driver.driverId, image:driver.url, name:driver.givenName+' '+driver.familyName}
+			});
+		}
+	}catch (err){
+		console.log("Driver not found");
+	}
+	
 	res.send(JSON.stringify(result));
 }
 
 module.exports = new Router()
 
   .get('/', (req, res) => {
-	lookupItem(req, res);
-    //res.sendStatus(200);
+	lookupItems(req, res);
   })
 	
   .get('/:id', (req, res) => {
-    // Code here
-    res.sendStatus(200);
+    lookupItemByID(req, res);
   });
